590dceae8687fe6c816a3448a7286653673dc507
RosieOh <20172207@gm.hannam.ac.kr>
2023-11-24 10:28:17 +0900

    🌈explain : spring boot Schedule & Comment🌈

---
+++ test01_4/src/main/java/com/shop/service/ItemService.java
@@ -0,0 +1,13 @@
+package com.shop.service;
+
+import com.shop.domain.Item;
+
+import java.util.List;
+
+public interface ItemService {
+    public int save(Item item);
+    public List<Item> findAll();
+    public Item findById(Long id);
+    public Item getLatestItem();
+    public void update(Long itemId, Item itemParam);
+}
