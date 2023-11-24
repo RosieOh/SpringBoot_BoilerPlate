f9240aac966d636f311fecf3ea739a468c72893b
RosieOh <20172207@gm.hannam.ac.kr>
2023-11-24 10:30:12 +0900

    🌈explain : spring boot & spring security🌈"

---
+++ test02/src/test/java/com/chunjae/test02/Test02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chunjae.test02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
